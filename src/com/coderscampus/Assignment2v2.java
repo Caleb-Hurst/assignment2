c165bac93bbddbd8c894c222d3962dd6f6473755
Caleb-Hurst <hursty.caleb@gmail.com>
2022-11-30 12:31:19 -0600

    assignment2

---
+++ src/com/coderscampus/Assignment2v2.java
@@ -0,0 +1,55 @@
+package com.coderscampus;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Assignment2v2 {
+
+	public static void main(String[] args) {
+			Assignment2v2 main = new Assignment2v2();
+			main.game();
+		}
+
+		void game() {
+			int userInput = 0; 
+			int attempts = 0;
+			int correctAnswer;
+			correctAnswer = generateRandom();
+//			System.out.println(correctAnswer);
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Please enter a number between 1 and 100");
+			while (userInput < 101 || userInput > 0) {
+				userInput = Integer.parseInt(scanner.nextLine());
+				if (userInput < correctAnswer || userInput < 0) {
+					System.out.println("Please pick a higher number");
+				}
+				if (userInput > correctAnswer || userInput > 101) {
+					System.out.println("Please pick a lower number");
+				}
+				if (userInput == correctAnswer) {
+					System.out.println("You win!!!");
+					break;
+				}
+				attempts++;
+				if (attempts >= 5) {
+					break;
+				}
+			}
+			if (userInput != correctAnswer) {
+				System.out.println("You lose, the number to guess was: " + correctAnswer);
+				
+			}
+			scanner.close();
+
+		}
+
+		private int generateRandom() {
+			int correctAnswer;
+			Random random = new Random(); // put in method 
+			correctAnswer = random.nextInt(100) + 1; // prohibits the number 0 from being created
+			return correctAnswer;
+		}
+	
+	}
+
+
