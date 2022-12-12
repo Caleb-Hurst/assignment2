65e691f51850f51e20e4793c1f382110eb96bfe4
Caleb-Hurst <hursty.caleb@gmail.com>
2022-12-12 08:59:52 -0600

    v 2 update
    fixed bug where an incorrect input would count against number of guesses the user has. should work well now.

--- src/com/coderscampus/Assignment2v2.java
+++ src/com/coderscampus/Assignment2v2.java
@@ -20,6 +20,10 @@ public class Assignment2v2 {
 			System.out.println("Please enter a number between 1 and 100");
 			while (userInput < 101 || userInput > 0) {
 				userInput = Integer.parseInt(scanner.nextLine());
+				while (userInput>100 || userInput <=0) {
+					System.out.println("your guess is not between 1 and 100, please try again");
+					userInput = Integer.parseInt(scanner.nextLine());
+				}
 				if (userInput < correctAnswer || userInput < 0) {
 					System.out.println("Please pick a higher number");
 				}
